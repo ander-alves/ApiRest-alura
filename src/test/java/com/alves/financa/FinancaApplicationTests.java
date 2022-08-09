e092199a0d58dfa1038de7e9767bff8f0209940e
Anderson <andso.alves@gmail.com>
2022-08-08 21:32:01 -0300

    Primeiro commit

---
+++ src/test/java/com/alves/financa/FinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.financa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
